efa73348b5723d80599086d8c8ba5a8410693106
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-11-13 21:23:05 -0500

    Add pictures to friend list

--- static/js/script.js
+++ static/js/script.js
@@ -58,7 +58,8 @@ function displayInstructions() {
     document.getElementById("login").style.display = "none";
     var all_friends_element = document.querySelector("#friendList ul");
     var instructions_element = document.createElement("p");
-    var instructions_text_element = document.createTextNode("WHICH ONE IS IT?");
+    instructions_element.className = "instructions";
+    var instructions_text_element = document.createTextNode("WHO IS IT?");
 
     instructions_element.appendChild(instructions_text_element);
     all_friends_element.appendChild(instructions_element);
@@ -68,16 +69,25 @@ function displayInstructions() {
 function addToFriendsList(thread) {
     // Vanilla JS, sorry
     var participants = thread.to.data;
-    var other_person = participants[0].name == user_data.name ? 1 : 0;
+    var other_person_index = participants[0].name == user_data.name ? 1 : 0;
+    var other_person = participants[other_person_index];
 
     var all_friends_element = document.querySelector("#friendList ul");
+
     var friend_list_element = document.createElement("li");
     var friend_link_element = document.createElement("a");
-    var friend_text_element = document.createTextNode(participants[other_person].name);
-
     friend_link_element.href = "#";
 
-    friend_link_element.appendChild(friend_text_element);
+    var friend_name_element = document.createElement("p");
+    friend_name_element.className = "friendName"
+
+    var friend_text_element = document.createTextNode(other_person.name);
+    var friend_image_element = document.createElement("img");
+    friend_image_element.src = "https://graph.facebook.com/" + other_person.id + "/picture?height=100&height=100";
+
+    friend_link_element.appendChild(friend_image_element);
+    friend_name_element.appendChild(friend_text_element);
+    friend_link_element.appendChild(friend_name_element);
     friend_list_element.appendChild(friend_link_element);
     all_friends_element.appendChild(friend_list_element);
 }
