edf778f6d4b1705c3ec0b77609162bc9554663c2
Philipp Steinmann <steinmann.philipp@yahoo.com>
2014-11-19 21:29:09 -0500

    Add some analysis, add displayResults(), little fixes

--- static/js/script.js
+++ static/js/script.js
@@ -17,19 +17,17 @@ var user_data;
 
 // Nice to have
 var friend_name = "";
+var friend_first_name;
 
 // Array of objects. Important properties: `created_time`, `from`, `message`
 var messages = [];
-var fromOther=[];
-var fromYou=[];
+var messagesFromOther=[];
+var messagesFromYou=[];
 
 // The messages used to entertain the user while we fetch messages & compute stuff.
 // We save the timer so that we can cancel it when the computation is finished.
 var loading_messages_timer;
 var loading_messages = ["Crunching Numbers...", "Contacting Friends...", "Reading Diaries...", "Analyzing Facial Expressions..."];
-
-
-
 
 var MAX_MESSAGES_TO_LOAD = 400;
 
@@ -84,8 +82,7 @@ function prepareFriendList() {
     // Vanilla JS, sorry
     document.getElementById("start-button").style.display = "none";
     var all_friends_element = document.querySelector("#friendList ul");
-    var instructions_element = document.createElement("p");
-    instructions_element.className = "instructions";
+    var instructions_element = document.createElement("h2");
     var instructions_text_element = document.createTextNode("WHO IS IT?");
 
     instructions_element.appendChild(instructions_text_element);
@@ -134,6 +131,7 @@ function listenForClicks() {
         var elem = all_links[i];
         elem.onclick = function() {
             friend_name = this.getAttribute("data-name");
+            friend_first_name = getFriendFirstName();
             var thread_index = this.getAttribute("data-thread-index");
             // "Crunching numbers", etc
             showLoadingScreen();
@@ -143,6 +141,10 @@ function listenForClicks() {
             analyzeMessages();
         }
     }
+}
+
+function getFriendFirstName() {
+    friend_first_name = friend_name.split(" ")[0];
 }
 
 function showLoadingScreen() {
@@ -219,19 +221,21 @@ function handleError(error) {
 }
 
 function analyzeMessages() {
-    //convertDatesToEpoch();
+    convertDatesToEpoch();
     sortMessagesByDate();
     splitMessagesBySender();
-    var fromOtherIndex=indexOther();
-    var fromTimeBetIndex=timeBetIndex();
-}
 
-function timeBetIndex(){
+    var messageRatioRating = analyzeMessageRatio();
+    var messageDelayRating = analyzeMessageDelays();
+    var messageContentRating = analyzeMessageContent();
+    //var messageTimesRating = analyzeMessageTimes();
 
+    processResults(messageRatioRating, messageDelayRating, messageContentRating);
 }
 
 // Print out time differences between messages
 // Most are positive, but some are negative!
+// This means it's not perfectly chronologically sorted.
 function debugMessageOrder() {
     timeDifferences = [];
     for (var i = 0; i < messages.length - 1; i++) {
@@ -244,7 +248,7 @@ function debugMessageOrder() {
 // Makes it hard to compare
 function convertDatesToEpoch() {
     messages = messages.map(function(message) { 
-        message.created_time = Date.parse(message.created_time);
+        message.created_time_epoch = Date.parse(message.created_time);
         return message;
     } );
 }
@@ -253,22 +257,136 @@ function sortMessagesByDate() {
     messages.sort(function(a, b) { return a - b });
 }
 
+// Split `messages` into two arrays, `messagesFromYou` and `messagesFromOther`
 function splitMessagesBySender(){
     messages.forEach(function(message) {
         if (message.from.name == friend_name){
-            fromOther.push(message);
+            messagesFromOther.push(message);
         }
         else {
-            fromYou.push(message);
+            messagesFromYou.push(message);
         }
     } );
 }
 
-function indexOther(){
-    var n=(fromOther.length*100)/messages.length;
-    return Math.floor(n/20)+1
+// Called by analyzeMessages(), returns 1-5
+// Looks at ratio of her messages to your messages
+function analyzeMessageRatio() {
+    var percentage = (messagesFromOther.length * 100) / messages.length;
+    // (it's 6 if she sent all messages, but that's unlikely)
+    var rating = Math.floor(percentage / 20) + 1;
+    return rating;
 
 }
+
+// Called by analyzeMessages(), returns 1-5
+function analyzeMessageDelays() {
+    return 2;
+}
+
+// Called by analyzeMessages(), returns 1-5
+// Abandoned for now due to time zone difficulties
+// Looks at the time of day during which you sent each other messages
+// The deeper into the night, the better
+function analyzeMessageTimes() {
+    // Defining 3 a.m. as the gold standard for nightly discussion
+    distances_to_3 = [];
+    messages.forEach(function(message) {
+        var distance_to_3_before = 1;
+        var distance_to_3_after = 1;
+        var closest_distance_to_3;
+    } );
+    return 0;
+}
+
+// Called by analyzeMessages(), returns 1-5
+function analyzeMessageContent() {
+    // Those count a lot
+    var strikingWords = ["I like you", "hot", ";)", ":p"];
+    // A colon followed by two characters that are not alphabet letters or spaces
+    var emoticonsRegex = /:[^\w\s]{1,2}/g;
+
+    var total_word_count = 0;
+    var striking_word_count = 0;
+    var emoticon_count = 0;
+    messagesFromOther.forEach(function(message) {
+        total_word_count++;
+        // Count striking words
+        strikingWords.forEach(function(strikingWord) {
+            var striking_word_index = message.message.indexOf(strikingWord);
+            while (striking_word_index != -1) {
+                striking_word_count++;
+                striking_word_index = message.message.indexOf(strikingWord, striking_word_index);
+            }
+        } );
+        
+        var emoticon_match = emoticonsRegex.exec(message.message);
+        while (emoticon_match != null) {
+            emoticon_count++;
+            emoticon_match = emoticonsRegex.exec(message.message);
+        }
+        // Reset the regex
+        emoticonsRegex.lastIndex = 0;
+    } );
+    console.log(total_word_count);
+    console.log(striking_word_count);
+    console.log(emoticon_count);
+    return 2;
+}
+
+// Not used after all, but might be useful later
+function getNumberOfWords(messageArray) {
+    var word_count = 0;
+    messageArray.forEach(function(message) {
+        var number_of_words = message.message.split(" ").length;
+        word_count += number_of_words;
+    } );
+    return word_count;
+}
+
+// Take the results, get ready to display them
+function processResults(messageRatioRating, messageDelayRating, messageContentRating) {
+    var averageRating = (messageRatioRating, messageDelayRating, messageContentRating) / 3;
+
+    // The timeout that switches the humurous loading messages
+    clearTimeout(loading_messages_timer);
+
+    // Convert 1-5 rating to a message for the user
+    var verdict = getVerdict(averageRating);
+
+    // Clear out previous HTML
+    clearContent();
+    displayResults(verdict);
+}
+
+// Clear out previous HTML
+function clearContent() {
+    var contentElement = document.getElementById("changing-content");
+    while (contentElement.hasChildNodes()) {
+        contentElement.removeChild(contentElement.lastChild);
+    }
+}
+
+// Map rating (1-5) to message for the user
+function getVerdict(rating) {
+    var verdicts = [
+        "We're not entirely sure that " + friend_first_name + " is aware of your existence.",
+        "Sorry, " + user_data.friend_name + ", things don't look so good. Hey, we'll buy you a beer.",
+        "We're not entirely sure...you and " + user_data.friend_name + " are a little close to be friends and a little distant to be into each other. Take care.",
+        "Things are looking good, " + user_data.friend_name + ". Go for it.",
+        "You and " + friend_first_name + " are a match made in heaven!"];
+    return verdicts[rating + 1];
+}
+
+function displayResults(verdict) {
+    var contentElement = document.getElementById("changing-content");
+    var verdictElement = document.createElement("h2");
+    var verdictTextElement = document.createTextNode(verdict);
+
+    verdictElement.appendChild(verdictTextElement);
+    contentElement.appendChild(verdictElement);
+}
+
 // http://stackoverflow.com/a/4033310/805556
 function getURL(url)
 {
